07b31ac354efe526e07dbfb02c00adc2d4e40326
Vladyslav Shkola <Vladyslav_Shkola@epam.com>
2017-05-10 03:25:57 +0300

    added linked list implementation

---
+++ DataStructures/DataStructures/LinkedList.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    class LinkedList<T> : IEnumerable<T>
+    {
+        //Implement linked list with such function set: Length, Add, AddAt, Remove, RemoveAt, ElementAt.
+        //Implement Ienumerable to make your list work with foeach
+        private Node<T> first;
+        private Node<T> last;
+        private int size;
+
+        public void AddToTheBeginning(T element)
+        {
+            Node<T> helperNode = first;
+            Node<T> newNode = new Node<T>(null, element, helperNode);
+            first = newNode;
+            if (helperNode == null)
+            {
+                last = newNode;
+            }
+            else
+            {
+                helperNode.prev = newNode;
+            }
+            size++;
+        }
+
+        public void AddToTheEnd(T element)
+        {
+            Node<T> helperNode = last;
+            Node<T> newNode = new Node<T>(helperNode, element, null);
+            last = newNode;
+            if (helperNode == null)
+            {
+                first = newNode;
+            }
+            else
+            {
+                helperNode.next = last;
+            }
+            size++;
+        }
+
+        public void AddAt(int position, T element)
+        {
+            if (position == 0)
+            {
+                AddToTheBeginning(element);
+            }
+            else
+            {
+                Node<T> prevNode = GetNodeAt(position).prev;
+                Node<T> newNode = new Node<T>(prevNode, element, prevNode.next);
+                prevNode.next = newNode;
+                size++;
+            }
+        }
+
+        public void Add(T element)
+        {
+            AddToTheEnd(element);
+        }
+
+        private Node<T> GetNodeAt(int position)
+        {
+            if (position < 0 || position > size - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            Node<T> node = first;
+            for (int i = 0; i < position; i++)
+            {
+                node = node.next;
+            }
+            return node;
+        }
+
+        public T ElementAt(int position)
+        {
+            return GetNodeAt(position).value;
+        }
+
+
+        public void RemoveFromTheBeginning()
+        {
+            if (GetLength() > 1)
+            {
+                first = first.next;
+                first.prev = null;
+                size--;
+            }
+            else if (GetLength() == 1)
+            {
+                LeaveEmtyList();
+                size--;
+            }
+        }
+
+        private void LeaveEmtyList()
+        {
+            first = null;
+            last = null;
+        }
+
+        public void RemoveFromTheEnd()
+        {
+            if (GetLength() > 1)
+            {
+                last = last.prev;
+                last.next = null;
+                size--;
+            }
+            else if (GetLength() == 1)
+            {
+                LeaveEmtyList();
+                size--;
+            }
+        }
+
+        public void RemoveAt(int position)
+        {
+            if (position < 0 || position >= size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                if (position == size - 1)
+                {
+                    RemoveFromTheEnd();
+                }
+                else if (position == 0)
+                {
+                    RemoveFromTheBeginning();
+                }
+                else if (position > 1)
+                {
+                    Node<T> nodeForDeletion = GetNodeAt(position);
+                    Node<T> prevNode = nodeForDeletion.prev;
+                    Node<T> nextNode = nodeForDeletion.next;
+                    prevNode.next = nextNode;
+                    nextNode.prev = prevNode;
+                    size--;
+                }
+                else if (position == 1)
+                {
+                    LeaveEmtyList();
+                    size--;
+                }
+            }
+
+        }
+
+        public int GetLength()
+        {
+            return size;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = first;
+            while (current != null)
+            {
+                yield return current.value;
+                current = current.next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class Node<T>
+        {
+            internal T value;
+            internal Node<T> prev;
+            internal Node<T> next;
+
+            public Node(Node<T> prev, T element, Node<T> next)
+            {
+                this.prev = prev;
+                this.value = element;
+                this.next = next;
+            }
+        }
+    }
+}
---
+++ DataStructures/DataStructures/LinkedListTests.cs
@@ -0,0 +1,153 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DataStructures
+{
+    [TestFixture]
+    class LinkedListTests
+    {
+        [Test]
+        public void AddAt_PositionLessThanSize_And_MoreThanZero()
+        {
+            LinkedList<int> list = new LinkedList<int>() { 1, 2, 3, 4, 5 };
+            list.AddAt(2, 100);
+            Assert.AreEqual(100, list.ElementAt(2));
+        }
+
+        [Test]
+        public void AddAt_Position_0()
+        {
+            LinkedList<int> list = new LinkedList<int>() { 1, 2, 3, 4, 5 };
+            list.AddAt(0, 100);
+            Assert.AreEqual(100, list.ElementAt(0));
+        }
+
+        [Test]
+        public void AddAt_LastPosition()
+        {
+            LinkedList<int> list = new LinkedList<int>() { 1, 2, 3, 4, 5 };
+            list.AddAt(list.GetLength() - 1, 100);
+            Assert.AreEqual(100, list.ElementAt(list.GetLength() - 2));
+        }
+
+        [Test]
+        public void AddAt_PositionMoreThanLastElement()
+        {
+            LinkedList<int> list = new LinkedList<int>() { 1, 2, 3, 4, 5 };
+            Assert.That(() => list.AddAt(list.GetLength(), 100), Throws.TypeOf<IndexOutOfRangeException>());
+        }
+
+        [Test]
+        public void AddAt_PositionLessThanZero()
+        {
+            LinkedList<int> list = new LinkedList<int>() { 1, 2, 3, 4, 5 };
+            Assert.That(() => list.AddAt(-1, 100), Throws.TypeOf<IndexOutOfRangeException>());
+        }
+
+        [Test]
+        public void RemoveAt_PositionLessThanSize_And_MoreThanZero()
+        {
+            LinkedList<int> list = new LinkedList<int>() { 1, 2, 3, 4, 5 };
+            int positionForDeleting = 2;
+            int newValue = list.ElementAt(positionForDeleting + 1);
+            int sizeBefore = list.GetLength();
+
+            list.RemoveAt(positionForDeleting);
+            Assert.IsTrue(list.ElementAt(positionForDeleting).Equals(newValue) && list.GetLength() == sizeBefore - 1);
+        }
+
+        [Test]
+        public void RemoveAt_Position_0()
+        {
+            LinkedList<int> list = new LinkedList<int>() { 1, 2, 3, 4, 5 };
+            int positionForDeleting = 0;
+            int newValue = list.ElementAt(positionForDeleting + 1);
+            int sizeBefore = list.GetLength();
+
+            list.RemoveAt(positionForDeleting);
+            Assert.IsTrue(list.ElementAt(positionForDeleting).Equals(newValue) && list.GetLength() == sizeBefore - 1);
+        }
+
+        [Test]
+        public void RemoveAt_LastPosition()
+        {
+            LinkedList<int> list = new LinkedList<int>() { 1, 2, 3, 4, 5 };
+            int positionForDeleting = 4;
+            int sizeBefore = list.GetLength();
+
+            list.RemoveAt(positionForDeleting);
+            Assert.IsTrue(list.GetLength() == sizeBefore - 1);
+        }
+
+        [Test]
+        public void RemoveAt_PositionMoreThanLastElement()
+        {
+            LinkedList<int> list = new LinkedList<int>() { 1, 2, 3, 4, 5 };
+            Assert.That(() => list.RemoveAt(list.GetLength()), Throws.TypeOf<IndexOutOfRangeException>());
+        }
+
+        [Test]
+        public void RemoveAt_PositionLessThanZero()
+        {
+            LinkedList<int> list = new LinkedList<int>() { 1, 2, 3, 4, 5 };
+            Assert.That(() => list.RemoveAt(-1), Throws.TypeOf<IndexOutOfRangeException>());
+        }
+
+        [Test]
+        public void AddToTheBeginningOfEmptyList()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            int value = 200;
+            list.AddToTheBeginning(value);
+
+            Assert.AreEqual(value, list.ElementAt(0));
+        }
+
+        [Test]
+        public void AddToTheEndOfEmptyList()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            int value = 200;
+            list.AddToTheEnd(value);
+
+            Assert.AreEqual(value, list.ElementAt(0));
+        }
+
+        [Test]
+        public void RemoveFromTheBeginningOfEmptyList()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.RemoveFromTheBeginning();
+
+            Assert.AreEqual(0, list.GetLength());
+        }
+
+        [Test]
+        public void RemoveFromTheBeginningOfListWithOneItem()
+        {
+            LinkedList<int> list = new LinkedList<int>() { 300 };
+            list.RemoveFromTheBeginning();
+
+            Assert.AreEqual(0, list.GetLength());
+        }
+
+        [Test]
+        public void RemoveFromTheEndOfEmptyList()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.RemoveFromTheEnd();
+
+            Assert.AreEqual(0, list.GetLength());
+        }
+
+        [Test]
+        public void RemoveFromTheEndOfListWithOneItem()
+        {
+            LinkedList<int> list = new LinkedList<int>() { 300 };
+            list.RemoveFromTheEnd();
+
+            Assert.AreEqual(0, list.GetLength());
+        }
+
+    }
+}
