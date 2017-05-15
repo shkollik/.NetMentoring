53a24d5384fcc377578cb8d26b7f1e3b5abb6b69
Vladyslav_Shkola <Vladyslav_Shkola@epam.com>
2017-05-15 14:09:17 +0300

    added Queue project

---
+++ DataStructures/Queue/Queue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    //FIFO structure implemented via LinkedList
+    class Queue<T> : IEnumerable<T>
+    {
+        LinkedList<T> _list;
+
+        public Queue()
+        {
+            _list = new LinkedList<T>();
+        }
+
+        //Add the specified item to the queue
+        public void Enqueue(T item)
+        {
+            _list.AddLast(item);
+        }
+
+        //Removes and returns the first item from the queue
+        public T Dequeue()
+        {
+            if(_list.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty");
+            }
+
+            T value = _list.First.Value;
+            _list.RemoveFirst();
+
+            return value;
+        }
+
+        //Returns the first item from the queue
+        public T Peek()
+        {
+            if (_list.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty");
+            }
+
+            return _list.First.Value;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _list.Count;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ DataStructures/Queue/QueueTests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class QueueTests
+    {
+        [Test]
+        public void TestEnqueu()
+        {
+            Queue<int> queue = new Queue<int>();
+            int value = 1;
+            queue.Enqueue(value);
+
+            Assert.AreEqual(value, queue.Peek());
+        }
+
+        [Test]
+        public void DequeueIfQueueIsNotEmptyReturnsTheFirstItem()
+        {
+            Queue<int> queue = new Queue<int>();
+            int firstValue = 1;
+            int secondValue = 2;
+            queue.Enqueue(firstValue);
+            queue.Enqueue(secondValue);
+
+            Assert.AreEqual(firstValue, queue.Dequeue());
+        }
+
+        [Test]
+        public void DequeueIfQueueIsNotEmptyRemovesFirstItem()
+        {
+            Queue<int> queue = new Queue<int>();
+            int firstValue = 1;
+            int secondValue = 2;
+            int countBeforeEnqueu = 2;
+
+            queue.Enqueue(firstValue);
+            queue.Enqueue(secondValue);
+            queue.Dequeue();
+
+            Assert.AreEqual(countBeforeEnqueu - 1, queue.Count);
+        }
+
+        [Test]
+        public void DequeueIfQueueIsEmpty()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            Assert.That(() => queue.Dequeue(), Throws.TypeOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void PeekIfQueueIsNotEmpty()
+        {
+            Queue<int> queue = new Queue<int>();
+            int firstValue = 1;
+            int secondValue = 2;
+            queue.Enqueue(firstValue);
+            queue.Enqueue(secondValue);
+
+            Assert.AreEqual(firstValue, queue.Dequeue());
+        }
+
+        [Test]
+        public void PeekIfQueueIsEmpty()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            Assert.That(() => queue.Peek(), Throws.TypeOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void TestCount()
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(200);
+
+            Assert.AreEqual(1, queue.Count);
+        }
+    }
+}
