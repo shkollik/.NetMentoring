5e879162237191909d0987055adbcc31792bc5f8
Vladyslav_Shkola <Vladyslav_Shkola@epam.com>
2017-05-15 13:21:56 +0300

    added Stack project

---
+++ DataStructures/Stack/Stack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    //LIFO structure implemented via LinkedList
+    class Stack<T> : IEnumerable<T>
+    {
+        private LinkedList<T> _list;
+        public Stack()
+        {
+            _list = new LinkedList<T>();
+        }
+
+        //Add the specified item to the stack
+        public void Push(T item)
+        {
+            _list.AddFirst(item);
+        }
+
+        //Removes and returns the first item from stack
+        public T Pop()
+        {
+            if(_list.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            T value = _list.First.Value;
+            _list.RemoveFirst();
+            return value;
+        }
+
+        //Returns the topt item from stack without removing it
+        public T Peek()
+        {
+            if (_list.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            return _list.First.Value;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _list.Count;
+            }
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ DataStructures/Stack/StackTests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Stack
+{
+    class StackTests
+    {
+        [Test]
+        public void TestPush()
+        {
+            Stack<int> stack = new Stack<int>();
+            int value = 1;
+            stack.Push(value);
+
+            Assert.AreEqual(value, stack.Peek());
+        }
+
+        [Test]
+        public void PopIfStackIsNotEmptyReturnsTheTopItem()
+        {
+            Stack<int> stack = new Stack<int>();
+            int firstValue = 1;
+            int secondValue = 2;
+            stack.Push(firstValue);
+            stack.Push(secondValue);
+
+            Assert.AreEqual(secondValue, stack.Pop());
+        }
+
+        [Test]
+        public void PopIfStackIsNotEmptyRemovesTheTopItem()
+        {
+            Stack<int> stack = new Stack<int>();
+            int firstValue = 1;
+            int secondValue = 2;
+            int countBeforePop = 2;
+            
+            stack.Push(firstValue);
+            stack.Push(secondValue);
+            stack.Pop();
+
+            Assert.AreEqual(countBeforePop-1, stack.Count);
+        }
+
+        [Test]
+        public void PopIfStackIsEmpty()
+        {
+            Stack<int> stack = new Stack<int>();
+
+            Assert.That(() => stack.Pop(), Throws.TypeOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void PeekIfStackIsNotEmpty()
+        {
+            Stack<int> stack = new Stack<int>();
+            int firstValue = 1;
+            int secondValue = 2;
+            stack.Push(firstValue);
+            stack.Push(secondValue);
+
+            Assert.AreEqual(secondValue, stack.Peek());
+        }
+
+        [Test]
+        public void PeekIfStackIsEmpty()
+        {
+            Stack<int> stack = new Stack<int>();
+
+            Assert.That(() => stack.Peek(), Throws.TypeOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void TestClear()
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(100);
+
+            stack.Clear();
+
+            Assert.AreEqual(0, stack.Count);
+        }
+
+        [Test]
+        public void TestCount()
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(100);
+            
+            Assert.AreEqual(1, stack.Count);
+        }
+    }
+}
